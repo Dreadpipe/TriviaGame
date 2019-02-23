90532790e683bfdda2e1ccb3d425b06fdc8eb274
Dreadpipe <benharris83@gmail.com>
2019-02-23 17:52:56 -0600

    pseudo-code and setup

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+// Document: ready jquery function
+
+// TO DO: I need a setInterval timer
+
+// that begins on a button push
+
+// that countsdown from 30
+
+// TO DO: I need an array of multiple choice questions
+
+// that are chosen randomly, and push to the HTML
+
+// and can be answered with buttons
+
+// which register as correct or incorrect
+
+// TO DO: I need a times up alert
+
+// that initializes the function
+
+// and resets the board and timer
